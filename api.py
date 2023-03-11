6268e76af173a02d558a35be9f959a37b0b6c61f
Kunal Kankariya <kkankariya007@gmail.com>
2023-03-11 23:03:52 +0530

    Api Done

--- api.py
+++ api.py
@@ -1,25 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import pymongo 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+app = FastAPI()
 
-client = pymongo.MongoClient("mongodb+srv://SaiHarshithTedla:1768%40Sh14035@cluster0.5ltvpng.mongodb.net/?retryWrites=true&w=majority")
-db = client.test
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-name="Kunal"
-age=21
-contact=8767046387
-email="kk3288@gmail.com"
+@app.post("/add")
+async def save(name:str,age:int,contact:int,email:str):
+  client = pymongo.MongoClient("mongodb+srv://SaiHarshithTedla:1768%40Sh14035@cluster0.5ltvpng.mongodb.net/?retryWrites=true&w=majority")
+  db = client.test
 
-Database = client["myFirstDatabase"]
-SampleTable = Database["Info"]
+  # name="Kunal"
+  # age=21
+  # contact=8767046387
+  # email="kk3288@gmail.com"
 
+  Database = client["myFirstDatabase"]
+  SampleTable = Database["Info"]
 
+  person = {
+    "name": name,
+    "age":age,
+    "contact": contact,
+    "email":email
+  }
 
-person = {
-  "name": name,
-  "age":age,
-  "contact": contact,
-  "email":email
-}
-SampleTable.insert_one(person)
+  SampleTable.insert_one(person)
+  return "Hogaya bhai"
